a530b25f3c70773739918d1defba068ae198519d
Jessica Blankemeier <jessicablankemeier@gmail.com>
2021-02-20 20:07:57 -0800

    Woot! Passing tests for Friday is Friday Scenario

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -2,17 +2,25 @@ const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
 
 function isItFriday(today) {
-   this.today = 'Friday';
+   if (today === "Friday"){
+       return "TGIF";
+   } else {
+       return "Nope";
+   }
   }
 
 Given('today is Sunday', function () {
     this.today = 'Sunday';
   });
 
+Given('today is Friday', function(){
+    this.today = 'Friday';
+})
+
   When('I ask whether it\'s Friday yet', function () {
     this.actualAnswer = isItFriday(this.today);
   });
 
   Then('I should be told {string}',  function (expectedAnswer) {
-    assert.equal(this.actualAnswer, expectedAnswer);
+    assert.strictEqual(this.actualAnswer, expectedAnswer);
   });
